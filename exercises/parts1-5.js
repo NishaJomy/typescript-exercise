2013b8267bb74bd5ace1106847ffd0d500dc2d05
Nisha George <nishajomy.2007@gmail.com>
2023-03-06 11:35:01 -0600

    modified ts file

--- exercises/parts1-5.js
+++ exercises/parts1-5.js
@@ -1,22 +1,15 @@
+"use strict";
 // URL for the instructions: 
 // https://education.launchcode.org/intro-to-professional-web-dev/chapters/typescript/exercises.html 
-
-import { SpaceLocation } from './SpaceLocation';
+exports.__esModule = true;
+var SpaceLocation_1 = require("./SpaceLocation");
 // Part 1: Declare (5) Variables With Type
-
-
 //let spacecraftName : string = 'Determination';
-
 //let speedMph : number = 17500;
-
-let kilometersToMars : number = 225000000;
-
-let kilometersToTheMoon : number = 384400;
-
+var kilometersToMars = 225000000;
+var kilometersToTheMoon = 384400;
 //let milesPerKilometer : number = 0.621;
-
 // Part 2:
-
 /*let milesToMars : number = kilometersToMars * milesPerKilometer ;
 
 let hoursToMars : number = milesToMars / speedMph ;
@@ -24,57 +17,39 @@ let hoursToMars : number = milesToMars / speedMph ;
 let daysToMars : number = hoursToMars / 24 ;
 
 console.log(`${spacecraftName} took ${daysToMars} days to Mars.`);*/
-
 // Part 3: Create a Function ("getDaysToLocation")
-
 /*function getDaysToLocation(kilometersAway: number): number {
     let milesAway: number = kilometersAway * milesPerKilometer;
     let hours: number = milesAway / speedMph;
     return hours / 24;
  }*/
- //console.log(`${spacecraftName} took ${getDaysToLocation(kilometersToMars) } to Mars.`);
- //console.log(`${spacecraftName} took ${getDaysToLocation(kilometersToTheMoon) } to moon.`);
-
+//console.log(`${spacecraftName} took ${getDaysToLocation(kilometersToMars) } to Mars.`);
+//console.log(`${spacecraftName} took ${getDaysToLocation(kilometersToTheMoon) } to moon.`);
 // Part 4: Create a Spacecraft Class
-
-class Spacecraft{
-
-        milesPerKilometer: number = 0.621;
-
-        name: string;
-
-        speedMph: number;
-
-    constructor(name: string, speedMph: number) {
+var Spacecraft = /** @class */ (function () {
+    function Spacecraft(name, speedMph) {
+        this.milesPerKilometer = 0.621;
         this.name = name;
         this.speedMph = speedMph;
     }
-
-    getDaysToLocation(kilometersAway: number): number {
-        let milesAway: number = kilometersAway * this.milesPerKilometer;
-        let hoursToMars: number = milesAway / this.speedMph;
+    Spacecraft.prototype.getDaysToLocation = function (kilometersAway) {
+        var milesAway = kilometersAway * this.milesPerKilometer;
+        var hoursToMars = milesAway / this.speedMph;
         return hoursToMars / 24;
-     }
-     printDaysToLocation(location: SpaceLocation) {
-        console.log(`${this.name} would take ${this.getDaysToLocation(location.kilometersAway)} days to get to ${location.name}.`);
-     }
-
-
-}
+    };
+    Spacecraft.prototype.printDaysToLocation = function (location) {
+        console.log("".concat(this.name, " would take ").concat(this.getDaysToLocation(location.kilometersAway), " days to get to ").concat(location.name, "."));
+    };
+    return Spacecraft;
+}());
 // Create an instance of the class here:
-
-let spaceShuttle = new Spacecraft('Determination', 17500);
-
+var spaceShuttle = new Spacecraft('Determination', 17500);
 // Move your output statements from part 3 here. Update the template literals use the
 // instance of the class.
-spaceShuttle.printDaysToLocation(new SpaceLocation('Mars', kilometersToMars));
-spaceShuttle.printDaysToLocation(new SpaceLocation('the Moon', kilometersToTheMoon));
+spaceShuttle.printDaysToLocation(new SpaceLocation_1.SpaceLocation('Mars', kilometersToMars));
+spaceShuttle.printDaysToLocation(new SpaceLocation_1.SpaceLocation('the Moon', kilometersToTheMoon));
 //console.log(`${spaceShuttle.name} took ${spaceShuttle.printDaysToLocation(new SpaceLocation('Mars', kilometersToMars)); }  days to Mars.`);
-
 // Part 5: Export and Import the SpaceLocation Class
 // Add the required import statement BEFORE the part 1 concent.
-
-
 // Add the printDaysToLocation function to the Spacecraft class.
-
 // Paste in the code from step 6 here:
